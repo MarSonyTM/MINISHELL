6776d6fc83be5b3c3b474d9cbb4c68cdb521e5ac
MarSonyTM <marsonyms@yahoo.com~>
2024-02-19 17:39:07 +0100

    update

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
- #include "../inc/minishell.h"
+#include "../inc/minishell.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
+    if (argc != 1 || argv[1])
+    {
+        printf("This program takes no arguments\n");
+        exit (0);
+    }
     char *input;
     while (1)
     {
