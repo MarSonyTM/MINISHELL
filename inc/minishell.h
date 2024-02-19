9c1c3ef3dc606b4c9856d07a4bbb7a9d346581e2
MarSonyTM <marsonyms@yahoo.com~>
2024-02-19 17:47:26 +0100

    2 structs added to .h

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,10 +6,29 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 # include "../libft/libft.h"
+# include <sys/wait.h>
+# include <fcntl.h>
 
 
 #define PROMPT "minishell> "
 
+/* holds information of each separate environment variable */
+typedef struct s_env
+{
+    char            *key;
+    char            *value;
+    struct s_env    *next;
+}   t_env;
 
+/* holds information of each separate command / child process */
+typedef struct s_cmd
+{
+    char            *cmd_path; //for execve, else NULL
+    char            **cmd_arr; //holds flags and arguments
+    char            *input; //for input redirection, else NULL
+    char            *output; //for output redirection, else NULL
+    int             exit_status;
+    struct s_cmd    *next;
+}   t_cmd;
 
 #endif
