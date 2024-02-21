823c46b6953761dbd425312b1d7a01a8b62d2326
MarSonyTM <marsonyms@yahoo.com~>
2024-02-21 11:16:32 +0100

    carla's structs

--- inc/minishell.h
+++ inc/minishell.h
@@ -2,7 +2,6 @@
 #define MINISHELL_H
 
 # include <stdio.h>
-# include <string.h>
 # include <stdlib.h>
 # include <readline/readline.h>
 # include <readline/history.h>
@@ -10,17 +9,26 @@
 # include <sys/wait.h>
 # include <fcntl.h>
 
+
 #define PROMPT "minishell> "
 
-/* holds information of each separate command / child process */
-typedef struct s_command {
-    char *cmd_path;
-    char **cmd_args;
-    char *input_file;
-    char *output_file;
-    int append_output;
-    struct s_command *next;
-} t_command;
+/* holds information of each separate environment variable */
+typedef struct s_env
+{
+    char            *key;
+    char            *value;
+    struct s_env    *next;
+}   t_env;
 
+/* holds information of each separate command / child process */
+typedef struct s_cmd
+{
+    char            *cmd_path; //for execve, else NULL
+    char            **cmd_arr; //holds flags and arguments
+    char            *input; //for input redirection, else NULL
+    char            *output; //for output redirection, else NULL
+    int             exit_status;
+    struct s_cmd    *next;
+}   t_cmd;
 
 #endif
