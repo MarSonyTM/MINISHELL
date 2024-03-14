2afac4840012fef70020a24d4afe777c0020b355
Carla Juliana Sturm <csturm@c4r5p3.42vienna.com>
2024-03-14 18:35:25 +0100

    made basic error logic

--- src/execution/executor.c
+++ src/execution/executor.c
@@ -20,10 +20,7 @@ static void	child_process(t_cmd *cmd, int i, t_exec *exec, t_env **env)
 		redirection(cmd->redirection_append, 2);
 	envp = env_to_array(cmd, env);
 	if (execve(cmd->cmd_path, cmd->cmd_arr, envp) == -1)
-	{
-		/* error ERROR*/
-		exit (1);
-	}
+		exit (cmd->exit_status);
 }
 
 static void	create_child_process(t_cmd *cmd, int i, t_exec *exec, t_env **env)
@@ -86,45 +83,37 @@ static void	init_exec(t_exec *exec, t_cmd *cmd, t_env **env)
 int	executor(t_cmd *cmd, t_env **env)
 {
 	t_exec	exec;
+	t_cmd	*tmp;
 	int		i;
 	int		j;
-	int		child_exit_status;
+	int		last_exit_status;
 
 	init_exec(&exec, cmd, env);
+	last_exit_status = 0;
 	i = 0; //initialize i to 0, siginifies process number
 	j = exec.processes; //initialize j to number of processes
+	tmp = cmd;
 	while (j > 0) //while there are still processes to execute
 	{
-		create_child_process(cmd, i, &exec, env);
+		create_child_process(tmp, i, &exec, env);
 		j--;
 		i++;
-		if (cmd->next != NULL) //if there are still commands to execute
-			cmd = cmd->next; //move to next command
+		if (tmp->next != NULL) //if there are still commands to execute
+			tmp = tmp->next; //move to next command
 	}
 	i = 0;
-while (i < exec.processes)
-{
-    if (exec.pid[i] != -1)
-    {
-        int status;
-        waitpid(exec.pid[i], &status, 0); // Wait for each child process to finish
-        
-        if (WIFEXITED(status))
-        {
-            // Capture the exit status of the child process
-            int exitStatus = WEXITSTATUS(status);
-            // Optionally, update the exit status for the shell to report
-            child_exit_status = exitStatus;
-            
-            // For debugging: Print the exit status of the child process
-            printf("Child process %d exited with status %d\n", exec.pid[i], exitStatus);
-        }
-    }
-    i++;
-}
-close_and_free(&exec);
-
-// Optionally: Use the exit status of the last child process for some logic
-// For now, simply return it or ignore it
-return child_exit_status; // Or just return 0 to indicate the shell itself exits normally
+	while (i < exec.processes)
+	{
+		if (exec.pid[i] != -1)
+		{
+			waitpid(exec.pid[i], &exec.status[i], 0); // Wait for each child process to finish
+			
+			if (WIFEXITED(exec.status[i]) && WEXITSTATUS(exec.status[i]) != 0)
+				last_exit_status = cmd->exit_status; // Capture the exit status of the child process
+		}
+		cmd = cmd->next;
+		i++;
+	}
+	close_and_free(&exec);
+	return (last_exit_status);
 }
