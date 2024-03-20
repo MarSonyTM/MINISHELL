781be5fa82c8600f21523393e462067f8b525e27
Marian Furnica <mafurnic@c4r4p10.42vienna.com>
2024-03-20 15:11:13 +0100

    Revert "lexer cleaning"
    This reverts commit 6aa651d3f7efd974150ccf159117bd1557b69b2f.

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -5,7 +5,8 @@ int lexer(char *input, t_token **tokens)
     int i = 0; // Index for input string
     char currentChar; // Current character being processed
     char *buffer = malloc((ft_strlen(input) + 1) * sizeof(char));
-    if (buffer == NULL) {
+    if (buffer == NULL) 
+    {
         // Handle memory allocation error
         // printf("Error: Failed to allocate memory for buffer\n");
         return (1);
@@ -14,68 +15,172 @@ int lexer(char *input, t_token **tokens)
     int inQuote = 0; // 0: no quote, 1: single quote, 2: double quote
     bool quote_error = false; // Track if there's an unclosed quote error
 
-    while ((currentChar = input[i]) != '\0' && !quote_error) {
-        if ((currentChar == ' ' || currentChar == '\t' || currentChar == '\n') && inQuote == 0) {
+    while ((currentChar = input[i]) != '\0' && !quote_error)
+    {
+        if ((currentChar == ' ' || currentChar == '\t' || currentChar == '\n') && inQuote == 0) 
+        {
             // Whitespace, outside of quotes
-            if (bufIndex > 0) {
-                handle_whitespace(buffer, &bufIndex, tokens, inQuote);
+            if (bufIndex > 0)
+             {
+                buffer[bufIndex] = '\0'; // Null-terminate the current token
+                if (add_token(tokens, determine_token_type(buffer, inQuote), ft_strdup(buffer)) == 1)
+                    return (1); // Add the token
+                bufIndex = 0; // Reset buffer index for the next token
             }
-        } else if (currentChar == '\'' || currentChar == '\"') {
+        } 
+        else if (currentChar == '\'' || currentChar == '\"') 
+        {
             // Quote handling
-            handle_quotes(buffer, &bufIndex, currentChar, &inQuote);
-        } else if (currentChar == ',' && inQuote == 0) {
+            if (inQuote == 0) 
+            { // Starting a quote
+                if (currentChar == '\'') 
+                    inQuote = 1;
+                else 
+                    inQuote = 2;
+                
+            } 
+            else if ((inQuote == 1 && currentChar == '\'') || (inQuote == 2 && currentChar == '\"'))
+                // Ending a quote
+                inQuote = 0;
+            else 
+                // Inside quotes, treat as part of the token
+                buffer[bufIndex++] = currentChar;
+        } 
+        else if (currentChar == ',' && inQuote == 0) 
+        {
             // Commas outside of quotes are treated as separate tokens
-            handle_comma(buffer, &bufIndex, tokens, inQuote);
-        } else if (currentChar == '<' && input[i + 1] != '<' && inQuote == 0) {
+            if (bufIndex > 0) 
+            {
+                // Add the current token before the comma
+                buffer[bufIndex] = '\0';
+                if (add_token(tokens, determine_token_type(buffer, inQuote), ft_strdup(buffer)) == 1) // Add the token
+                    return (1); // Error
+                bufIndex = 0;
+            }
+            // Add the comma as a separate token
+            if (add_token(tokens, TOKEN_COMMA, ft_strdup(",")) == 1) // Add the token
+                return (1); // Error
+        } 
+        else if (currentChar == '<' && input[i + 1] != '<' && inQuote == 0)
+         {
             // Check if the next character is not '<' and not in a quote
             // This indicates a redirect in token
-            handle_redirect_in(buffer, &bufIndex, tokens, inQuote);
-        } else if (currentChar == '>' && input[i + 1] != '>' && inQuote == 0) {
+            if (bufIndex > 0) 
+            {
+                buffer[bufIndex] = '\0'; // Null-terminate the current token
+                if (add_token(tokens, determine_token_type(buffer, inQuote), ft_strdup(buffer)) == 1) // Add the token
+                    return (1); // Error
+                bufIndex = 0; // Reset buffer index for the next token
+            }
+            // Add the redirect in token
+            if (add_token(tokens, TOKEN_REDIRECT_IN, ft_strdup("<")) == 1) // Add the token
+                return (1); // Error
+        } 
+        else if (currentChar == '>' && input[i + 1] != '>' && inQuote == 0)
+        {
             // Check if the next character is not '>' and not in a quote
             // This indicates a redirect out token
-            handle_redirect_out(buffer, &bufIndex, tokens, inQuote);
-        } else if (currentChar == '$' && input[i + 1] == '?') {
-            // Check if the next character is '?' and not in a quote
-            // This indicates an exit status token
-            handle_exit_status(buffer, &bufIndex, tokens, inQuote, &i);
-        } else if (currentChar == '$' && input[i + 1] == '\0') {
-            add_token(tokens, TOKEN_ARG, ft_strdup("$"));
-        } else if (currentChar == '$' && ft_isalpha(input[i + 1]) == 0) {
-            buffer[bufIndex] = '\0';
-        } else if (currentChar == '<' && input[i + 1] == '<' && inQuote == 0) {
-            // Check if the next character is also '<' and not in a quote
-            // This indicates a heredoc token
-            handle_heredoc(buffer, &bufIndex, tokens, inQuote, &i);
-        } else if (currentChar == '>' && input[i + 1] == '>' && inQuote == 0) {
-            // Check if the next character is also '>' and not in a quote
-            // This indicates a redirect out append token
-            handle_redirect_out_append(buffer, &bufIndex, tokens, inQuote, &i);
-        } else {
-            // Regular character, add to the buffer
-            buffer[bufIndex++] = currentChar;
-        }
-        i++;
-    }
-
-    if (bufIndex > 0 && !quote_error) {
-        if (inQuote != 0) {
-            error(ERR_QUOT, NULL);
-            quote_error = true;
-        } else {
-            buffer[bufIndex] = '\0';
-            if (add_token(tokens, determine_token_type(buffer, inQuote), ft_strdup(buffer)) == 1)
+            if (bufIndex > 0)
+            {
+                buffer[bufIndex] = '\0'; // Null-terminate the current token
+                if (add_token(tokens, determine_token_type(buffer, inQuote), ft_strdup(buffer)) == 1) // Add the token
+                    return (1); // Error
+                bufIndex = 0; // Reset buffer index for the next token
+            }
+            // Add the redirect out token
+            if (add_token(tokens, TOKEN_REDIRECT_OUT, ft_strdup(">")) == 1) // Add the token
                 return (1); // Error
         }
-    } else if (inQuote != 0 && !quote_error) {
+        else if (currentChar == '$' && input[i + 1] == '?')
+        {
+            // Check if the next character is '?' and not in a quote
+            // This indicates an exit status token
+            if (bufIndex > 0) 
+            {
+                buffer[bufIndex] = '\0'; // Null-terminate the current token
+                if (add_token(tokens, determine_token_type(buffer, inQuote), ft_strdup(buffer)) == 1) // Add the token
+                    return (1); // Error
+                bufIndex = 0; // Reset buffer index for the next token
+            }
+            // Add the exit status token
+            if (add_token(tokens, TOKEN_EXIT_STATUS, ft_strdup("$?")) == 1) // Add the token
+                return (1); // Error
+            i++; // Move past the '?'
+        }
+        else if (currentChar == '$' && input[i + 1] == '\0')
+        {
+            add_token(tokens, TOKEN_ARG, ft_strdup("$"));
+        }
+        else if (currentChar == '$' && ft_isalpha(input[i + 1]) == 0)
+        {
+            buffer[bufIndex] = '\0';
+        }
+        else if (currentChar == '<' && input[i + 1] == '<' && inQuote == 0)
+        {
+            // Check if the next character is also '<' and not in a quote
+            // This indicates a heredoc token
+            if (bufIndex > 0) 
+            {
+                buffer[bufIndex] = '\0'; // Null-terminate the current token
+                if (add_token(tokens, determine_token_type(buffer, inQuote), ft_strdup(buffer)) == 1) // Add the token
+                    return (1); // Error
+                bufIndex = 0; // Reset buffer index for the next token
+            }
+            // Add the heredoc token
+            if (add_token(tokens, TOKEN_HEREDOC, ft_strdup("<<")) == 1) // Add the token
+                return (1); // Error
+            i++; // Move past the second '<'
+        } 
+        else if (currentChar == '>' && input[i + 1] == '>' && inQuote == 0)
+        {
+            // Check if the next character is also '>' and not in a quote
+            // This indicates a redirect out append token
+            if (bufIndex > 0)
+         {
+                buffer[bufIndex] = '\0'; // Null-terminate the current token
+                if (add_token(tokens, determine_token_type(buffer, inQuote), ft_strdup(buffer)) == 1) // Add the token
+                    return (1); // Error
+                bufIndex = 0; // Reset buffer index for the next token
+            }
+            // Add the redirect out append token
+            if (add_token(tokens, TOKEN_REDIRECT_OUT_APPEND, ft_strdup(">>")) == 1) // Add the token
+                return (1); // Error
+            i++; // Move past the second '>'
+        }
+        else
+            // Regular character, add to the buffer
+            buffer[bufIndex++] = currentChar;
+        i++;
+    }
+    // Check if there's a token at the end of the input
+    if (bufIndex > 0 && !quote_error) 
+    {
+        if (inQuote != 0) 
+        { // If we ended in a quote, it's an unclosed quote error
+            error(ERR_QUOT, NULL);
+            quote_error = true; // Set error to prevent adding the token
+        } 
+        else 
+        {
+            buffer[bufIndex] = '\0'; // Null-terminate the current token
+            if (add_token(tokens, determine_token_type(buffer, inQuote), ft_strdup(buffer)) == 1) // Add the token
+                return (1); // Error
+        }
+    } 
+    else if (inQuote != 0 && !quote_error) 
+    {
+        // Handle the case where the input ends while still in a quote
         error(ERR_QUOT, NULL);
-        quote_error = true;
+        quote_error = true; // Set error to prevent adding the token
     }
-
-    if (quote_error) {
-        free(buffer);
-        return (1);
+    if (quote_error) 
+    {
+        // Free the tokens if there was an error
+        //  free_tokens(tokens); //segfaults here!!!!
+         free(buffer);
+        return (1); // Return without processing the tokens
     }
-
     return (0);
 }
 
+ 
--- src/lexer/lexer_utils.c
+++ src/lexer/lexer_utils.c
@@ -46,39 +46,3 @@ int	add_token(t_token **tokens, t_token_type type, char *value)
 	}
 	return (0);
 }
-
-int handle_whitespace(char *buffer, int *bufIndex, t_token **tokens, int inQuote) {
-    buffer[*bufIndex] = '\0'; // Null-terminate the current token
-    if (add_token(tokens, determine_token_type(buffer, inQuote), ft_strdup(buffer)) == 1)
-        return (1); // Add the token
-    *bufIndex = 0; // Reset buffer index for the next token
-    return (0);
-}
-
-int handle_quotes(char *buffer, int *bufIndex, char currentChar, int *inQuote) {
-    if (*inQuote == 0) { // Starting a quote
-        if (currentChar == '\'')
-            *inQuote = 1;
-        else
-            *inQuote = 2;
-    } else if ((*inQuote == 1 && currentChar == '\'') || (*inQuote == 2 && currentChar == '\"'))
-        // Ending a quote
-        *inQuote = 0;
-    else
-        // Inside quotes, treat as part of the token
-        buffer[(*bufIndex)++] = currentChar;
-    return (0);
-}
-
-int handle_comma(char *buffer, int *bufIndex, t_token **tokens, int inQuote) {
-    if (*bufIndex > 0) {
-        buffer[*bufIndex] = '\0';
-        if (add_token(tokens, determine_token_type(buffer, inQuote), ft_strdup(buffer)) == 1)
-            return (1); // Error
-        *bufIndex = 0;
-    }
-    if (add_token(tokens, TOKEN_COMMA, ft_strdup(",")) == 1)
-        return (1); // Error
-    return (0);
-}
-
