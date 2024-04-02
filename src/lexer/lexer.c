c899a1eeae0e2c9cd9891b0d2e443258d1cf6399
Marian Furnica <mafurnic@c4r4p10.42vienna.com>
2024-04-02 16:56:35 +0200

    lexer.c norminette not 100%

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -1,25 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mafurnic <mafurnic@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/02 16:45:34 by mafurnic          #+#    #+#             */
+/*   Updated: 2024/04/02 16:55:36 by mafurnic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../inc/minishell.h"
 
-int	lexer(char *input, t_token **tokens, t_env *env) 
+int	lexer(char *input, t_token **tokens, t_env *env)
 {
-	int		i; // Index for the input string
-	char *buffer; // Buffer to store the current token
-	int TokenCount; // Number of tokens
-	int bufIndex; // Index for the buffer
-	int inQuote; // Flag to indicate if we are in a quote
-	bool quote_error; // Flag to indicate if there was a quote error
-	i = 0; // Initialize the index to 0
-	buffer = malloc((ft_strlen(input) + 1) * sizeof(char)); // Allocate memory for the buffer
-	TokenCount = 0; // Initialize the token count to 0
-	bufIndex = 0; // Initialize the buffer index to 0
-	inQuote = 0; // Initialize the inQuote flag to 0
-	quote_error = false; // Initialize the quote error flag to false
-
-    if (buffer == NULL) // Check for allocation error
-        return (1);
-
-    // Process the input string
-    process_input_loop(input, &buffer, &bufIndex, &tokens, &TokenCount, env, &i, &inQuote, &quote_error);
-    // Finalize the lexer
-	return (finalize_lexer(&buffer, bufIndex, &tokens, &TokenCount, inQuote, quote_error, env));
+	int		i;
+	char	*buffer;
+	int		token_count;
+	int		buf_index;
+	int		in_quote;
+	bool	quote_error;
+	i = 0;
+	buffer = malloc((ft_strlen(input) + 1) * sizeof(char));
+	token_count = 0;
+	buf_index = 0;
+	in_quote = 0;
+	quote_error = false;
+	if (buffer == NULL)
+		return (1);
+	process_input_loop(input, &buffer, &buf_index, &tokens, &token_count, env, &i, &in_quote, &quote_error);
+	return (finalize_lexer(&buffer, buf_index, &tokens, &token_count, in_quote, quote_error, env));
 }
