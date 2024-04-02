64a1547d905116b72c1ad2aecf2e196e898d813f
Marian Furnica <mafurnic@c4r4p10.42vienna.com>
2024-04-02 16:55:06 +0200

    norminette update

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mafurnic <mafurnic@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/02 16:53:08 by mafurnic          #+#    #+#             */
+/*   Updated: 2024/04/02 16:53:34 by mafurnic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../inc/minishell.h"
 
 // Main parse function
-int parse(t_token *tokens, t_cmd **cmd, t_env *env)
+int	parse(t_token *tokens, t_cmd **cmd, t_env *env)
 {
 	processTokens(tokens, cmd, env);
-    return (0);
-} 
+	return (0);
+}
