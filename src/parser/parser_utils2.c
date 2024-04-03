2326be86b55a6858c21c1a01ff74a45f7060758d
Marian Furnica <mafurnic@c4r4p10.42vienna.com>
2024-04-03 15:13:26 +0200

    the issue has been found while cleaning for nomrinette in parser_utils2.c , something went wrong and the pipe did not work as it should

--- src/parser/parser_utils2.c
+++ src/parser/parser_utils2.c
@@ -6,30 +6,27 @@
 /*   By: mafurnic <mafurnic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 15:07:12 by mafurnic          #+#    #+#             */
-/*   Updated: 2024/04/03 15:07:45 by mafurnic         ###   ########.fr       */
+/*   Updated: 2024/04/03 15:12:23 by mafurnic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-// Adds the exit status token to the command's argument list.
-int handle_exit_status_token(t_cmd *current_cmd, char *value, int *arg_count)
+
+int	handle_exit_status_token(t_cmd *current_cmd, char *value, int *arg_count)
 {
-    if (!current_cmd) return 1; // Ensure there is a current command to work with.
-
-    // Increase the size of the command's argument array to accommodate the new argument.
-    current_cmd->cmd_arr = realloc(current_cmd->cmd_arr, sizeof(char *) * (*arg_count + 2));
-    if (!current_cmd->cmd_arr) return 1; // Check for allocation failure.
-
-    // Add the exit status token as a new argument.
-    current_cmd->cmd_arr[*arg_count] = ft_strdup(value);
-    if (!current_cmd->cmd_arr[*arg_count]) return 1; // Check for strdup failure.
-
-    // Increment the argument count and ensure the array is NULL-terminated.
-    *arg_count += 1;
-    current_cmd->cmd_arr[*arg_count] = NULL;
-
-    return 0; // Success.
+	if (!current_cmd)
+		return (1);
+	current_cmd->cmd_arr = realloc(current_cmd->cmd_arr,
+			sizeof(char *) * (*arg_count + 2));
+	if (!current_cmd->cmd_arr)
+		return (1);
+	current_cmd->cmd_arr[*arg_count] = ft_strdup(value);
+	if (!current_cmd->cmd_arr[*arg_count])
+		return (1);
+	*arg_count += 1;
+	current_cmd->cmd_arr[*arg_count] = NULL;
+	return (0);
 }
 
 t_cmd *handle_pipe_token(t_token **current, t_cmd **cmd, t_env *env, int *arg_count)
