feae499dc86d886f072ec1ed71359818211d5ee6
Marian Furnica <mafurnic@c4r4p10.42vienna.com>
2024-04-03 15:21:01 +0200

    function handle pipe token is cleaned in parser_utils2.

--- src/parser/parser_utils2.c
+++ src/parser/parser_utils2.c
@@ -6,7 +6,7 @@
 /*   By: mafurnic <mafurnic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 15:07:12 by mafurnic          #+#    #+#             */
-/*   Updated: 2024/04/03 15:17:17 by mafurnic         ###   ########.fr       */
+/*   Updated: 2024/04/03 15:20:22 by mafurnic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,28 +29,30 @@ int	handle_exit_status_token(t_cmd *current_cmd, char *value, int *arg_count)
 	return (0);
 }
 
-t_cmd *handle_pipe_token(t_token **current, t_cmd **cmd, t_env *env, int *arg_count)
+t_cmd	*handle_pipe_token(t_token **current,
+	t_cmd **cmd, t_env *env, int *arg_count)
 {
-    // Advance to the next token and use its value as the command for the new command structure
-    *current = (*current)->next;
-    if (*current == NULL || ((*current)->type != TOKEN_COMMAND && (*current)->type != TOKEN_BUILTIN)) 
-        return (error(ERR_PARS, "\n"), NULL);
-    // Create a new command structure for the next command in the pipeline
-    t_cmd *current_cmd = new_command(cmd);
-    if (!current_cmd)
-        return (NULL);
-    current_cmd->cmd_arr[0] = ft_strdup((*current)->value); // Set the command for the new command structure
-    if (!current_cmd->cmd_arr[0])
-        return (NULL);
-    current_cmd->cmd_arr[1] = NULL; // NULL terminate the array
-    // Resolve the command's path
-    char *cmd_path = resolve_command_path((*current)->value, env);
-    if (!cmd_path) 
-        return (NULL);
-    current_cmd->cmd_path = cmd_path;
-    // Reset argument count for the new command
-    *arg_count = 1;
-    return (current_cmd);
+	t_cmd	*current_cmd;
+	char	*cmd_path;
+
+	*current = (*current)->next;
+	if (*current == NULL
+		|| ((*current)->type != TOKEN_COMMAND
+			&& (*current)->type != TOKEN_BUILTIN))
+		return (error(ERR_PARS, "\n"), NULL);
+	current_cmd = new_command(cmd);
+	if (!current_cmd)
+		return (NULL);
+	current_cmd->cmd_arr[0] = ft_strdup((*current)->value);
+	if (!current_cmd->cmd_arr[0])
+		return (NULL);
+	current_cmd->cmd_arr[1] = NULL;
+	cmd_path = resolve_command_path((*current)->value, env);
+	if (!cmd_path)
+		return (NULL);
+	current_cmd->cmd_path = cmd_path;
+	*arg_count = 1;
+	return (current_cmd);
 }
 
 int process_tokens(t_token *tokens, t_cmd **cmd, t_env *env)
