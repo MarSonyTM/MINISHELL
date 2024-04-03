8413cbdb2574020f3a87a4712c1e0d605129f5dc
Marian Furnica <mafurnic@c4r4p10.42vienna.com>
2024-04-03 17:29:56 +0200

    norminette still needs work , too many arguments in function call too, plus comma issue outside of quotes

--- src/parser/resolve_command_path.c
+++ src/parser/resolve_command_path.c
@@ -6,13 +6,13 @@
 /*   By: mafurnic <mafurnic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 16:53:44 by mafurnic          #+#    #+#             */
-/*   Updated: 2024/04/03 15:09:56 by mafurnic         ###   ########.fr       */
+/*   Updated: 2024/04/03 17:29:06 by mafurnic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-char    *resolve_command_path(char *command, t_env *env) 
+char    *resolve_command_path(char *command, t_env *env)
 {
     char *path = ft_getenv("PATH", env); // Get the PATH environment variable value
     char *pathCopy = ft_strdup(path); // Duplicate since strtok modifies the string
