22983c67aac32fa945e15f6b3784050201cafe41
Marian Furnica <mafurnic@c4r4p10.42vienna.com>
2024-04-08 13:33:59 +0200

    cleaning header

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mafurnic <mafurnic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 11:16:13 by mafurnic          #+#    #+#             */
-/*   Updated: 2024/04/08 12:44:48 by mafurnic         ###   ########.fr       */
+/*   Updated: 2024/04/08 13:33:28 by mafurnic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -154,8 +154,10 @@ int				process_single_redirect_in(char *buffer,
 					t_token ***tokens, t_lexer *lexer);
 int				handle_builtin_or_command(t_cmd **cmd,
 					t_token *current, t_cmd **current_cmd, int *arg_count);
-int				finalize_buffer_and_add_token(char **buffer, t_token ***tokens, t_lexer *lexer, char *tokenValue);
-int				finalize_lexer(char **buffer, t_token ***tokens, t_lexer *lexer, bool quote_error);
+int				finalize_buffer_and_add_token(char **buffer,
+					t_token ***tokens, t_lexer *lexer, char *tokenValue);
+int				finalize_lexer(char **buffer,
+					t_token ***tokens, t_lexer *lexer, bool quote_error);
 int				process_input_loop(char *input, char **buffer,  t_token ***tokens, t_lexer *lexer, bool *quote_error); /**/
 void			process_dollar_conditions(char *input, char **buffer, t_token ***tokens, t_lexer *lexer);
 
