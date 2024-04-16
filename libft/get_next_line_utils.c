c1a0b013defccf1a0a30f475e20e9535e159bcae
Marian Furnica <mafurnic@c4r4p10.42vienna.com>
2024-04-16 13:58:29 +0200

    heredoc

---
+++ libft/get_next_line_utils.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mafurnic <mafurnic@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 13:53:33 by mafurnic          #+#    #+#             */
+/*   Updated: 2024/04/16 13:54:43 by mafurnic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*substr;
+
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	substr = (char *)malloc((len + 1) * sizeof(char));
+	if (!substr)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start + i])
+	{
+		substr[i] = s[start + i];
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
+
+unsigned long	ft_strlen(const char *s)
+{
+	unsigned long	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strdup(const char *s)
+{
+	int		i;
+	int		len;
+	char	*d;
+
+	i = 0;
+	len = ft_strlen(s);
+	d = (char *)malloc(sizeof(char) * (len + 1));
+	if (d == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	d[i] = '\0';
+	return (d);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*joined_str;
+	size_t	i;
+	size_t	len_s1;
+	size_t	len_s2;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	joined_str = (char *)malloc((len_s1 + len_s2 + 1) * sizeof(char));
+	if (!joined_str)
+		return (NULL);
+	i = 0;
+	while (*s1)
+		joined_str[i++] = *s1++;
+	while (*s2)
+		joined_str[i++] = *s2++;
+	joined_str[i] = '\0';
+	return (joined_str);
+}
