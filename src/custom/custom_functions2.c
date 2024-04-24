850bb376d4fa2443fffb8733446b27082eeadeba
Carla Juliana Sturm <csturm@c2r2p3.42vienna.com>
2024-04-24 17:50:30 +0200

    new structure for exit_cmd, but not finished

--- src/custom/custom_functions2.c
+++ src/custom/custom_functions2.c
@@ -6,7 +6,7 @@
 /*   By: csturm <csturm@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 15:50:20 by csturm            #+#    #+#             */
-/*   Updated: 2024/04/17 17:01:09 by csturm           ###   ########.fr       */
+/*   Updated: 2024/04/24 17:50:01 by csturm           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,12 +36,11 @@ int	exit_cmd(t_cmd *cmd, t_env *env, t_exec **exec, int stdout_fd)
 	int	exit_code;
 
 	exit_code = EXIT_SUCCESS;
-	if (cmd->cmd_arr[1] && cmd->cmd_arr[2])
-		return (error(ERR_ARGS, "exit", NULL, 1), 1);
-	else if (cmd->cmd_arr[1])
+	if (cmd->cmd_arr[1])
 	{
 		if (check_for_digit(cmd->cmd_arr[1]))
 		{
+			/* check for int overflow */
 			exit_code = ft_atoi(cmd->cmd_arr[1]);
 			while (exit_code < 0)
 				exit_code = 256 + exit_code;
@@ -51,6 +50,8 @@ int	exit_cmd(t_cmd *cmd, t_env *env, t_exec **exec, int stdout_fd)
 		else
 			exit_code = 2;
 	}
+	if (cmd->cmd_arr[1] && cmd->cmd_arr[2])
+		return (error(ERR_ARGS, "exit", NULL, 1), 1);
 	clean_up(cmd, env);
 	close_and_free(*exec);
 	ft_putendl_fd("exit", 1);
